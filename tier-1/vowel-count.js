2812a633a24d914bc125be55f9e4c4c0cd58afb5
jlsantia21 <jlsantia21@gmail.com>
2024-09-18 15:26:33 -0700

    Completed vowel-count.js code for Tier1 of AEP Assessment.

--- tier-1/vowel-count.js
+++ tier-1/vowel-count.js
@@ -4,4 +4,24 @@
 */
 module.exports = function(str){
   //Solution goes here
+
+  // vowel map used to keep track of vowel count
+  const vowelMap = new Map([
+    ["a", 0],
+    ["e", 0],
+    ["i", 0],
+    ["o", 0],
+    ["u", 0]
+  ]);
+  console.log(vowelMap)
+
+  // iterate through chars of input string
+  for(char of str){
+    if(vowelMap.has(char)){ // if the char is a vowel
+      vowelMap.set(char, vowelMap.get(char) + 1);
+    }
+  }
+
+  // return the sum of the vowels stored in vowelMap
+  return vowelMap.get("a") + vowelMap.get("e") + vowelMap.get("i") + vowelMap.get("o") + vowelMap.get("u");
 }
