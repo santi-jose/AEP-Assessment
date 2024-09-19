69bf1033e061935ece41f20539d4adfb8ebfc0e2
jlsantia21 <jlsantia21@gmail.com>
2024-09-18 20:24:24 -0700

    Completed pascals-triangle.js problem of AEP-Assessment Tier3.

--- tier-3/pascals-triangle.js
+++ tier-3/pascals-triangle.js
@@ -10,40 +10,34 @@ module.exports = function (row) {
 
   // array of arrays to return
   let rows = [row]; // array of rows with row rows
-  let newRow = []; // array of numbers
 
-  // if we are given an input of 1 rows
-  if (row === 1) {
-    // create a pascal triangle of 1 row
-    newRow[0] = 1;
-    rows[0] = newRow;
-  } else {
-    // iterate through input number of rows to create
-    for (let i = 0; i < row; i++) {
-      console.log(`i: ${i}`);
+  // iterate through row number of rows to create
+  for (let i = 0; i < row; i++) {
 
-      // create newRow variable to store new row to add to rows
-      newRow = [];
+      // initialize our new row as empty
+      let newRow = [];
 
       // iterate through digits to add to newRow
-      for (let j = 0; j < i; j++) {
+      for (let j = 0; j <= i; j++) {
         console.log(`j: ${j}`);
         // if we are at the first or last digit in the row
-        if (j === 0 || j === i - 1) {
+        if (j === 0 || j === i) {
           // set the value to 1
           newRow[j] = 1;
         } else {
           // else we are in the middle of the row
           // calculate the new value to add to the row
+          // newRow @ index j = 
+          // previous rows previous (i-1)'s previous digit (j-1) 
+          // + previous rows (i-1)'s current digit (j)
           newRow[j] = rows[i - 1][j - 1] + rows[i - 1][j];
         }
-
-        console.log(`new number: ${newRow[j]}`);
       }
 
-      rows[i] = newRow;
-    }
+    // add new row to our array of rows
+    rows[i] = newRow;
   }
 
+  // return array of rows
   return rows;
 };
